f8a778106c81c12a92fc9d9fc39d0c78449cb2a1
Denilson Telaroli <ddts80@gmail.com>
2015-03-06 22:59:30 -0300

    Added list_products to share

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,6 +2,7 @@ class Share < ActiveRecord::Base
   include ActiveUUID::UUID
   belongs_to :user
   belongs_to :list
+  has_many :list_products, through: :list
 
   validates :user_id, :list_id, presence: true
 end
--- app/views/shares/index.json.jbuilder
+++ app/views/shares/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@shares) do |share|
-  json.extract! share, :id, :user, :list
+  json.extract! share, :id, :user, :list, :list_products
   json.url share_url(share, format: :json)
 end
