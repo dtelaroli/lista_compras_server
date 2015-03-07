6d5ffae150490a9b53e0ee48964f4e8db15fd9e2
Denilson Telaroli <ddts80@gmail.com>
2015-03-06 22:21:21 -0300

    Ffix share migration

--- db/migrate/20150307001912_create_shares.rb
+++ db/migrate/20150307001912_create_shares.rb
@@ -2,11 +2,11 @@ class CreateShares < ActiveRecord::Migration
   def change
     create_table :shares do |t|
       t.references :user, index: true
-      t.uuid :list, index: true
+      t.uuid :list_id, index: true
 
       t.timestamps null: false
     end
     add_foreign_key :shares, :users
-    add_foreign_key :shares, :lists
+    add_foreign_key :shares, :lists, column: :list_id
   end
 end
